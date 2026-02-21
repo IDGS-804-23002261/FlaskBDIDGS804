25930ea3d07323d26c81322d0d388f0f9cf90dd0
Jimena <85547@alumnos.utleon.edu.mx>
2026-02-21 00:41:30 -0600

    detalles 20-02-26

--- app.py
+++ app.py
@@ -23,9 +23,29 @@ def index():
 	alumno=Alumnos.query.all()
 	return render_template("index.html", form=create_form, alumno=alumno)
 
-@app.route("/Alumnos")
+@app.route("/Alumnos", methods=['GET','POST'])
 def alumnos():
-	return render_template("Alumnos.html")
+	create_form=forms.UserForm(request.form)
+	if request.method=='POST':
+		alum=Alumnos(nombre=create_form.nombre.data, apaterno=create_form.apaterno.data, email=create_form.correo.data)
+		db.session.add(alum)
+		db.session.commit()
+		return redirect(url_for('index'))
+	return render_template("Alumnos.html", form=create_form)
+
+@app.route("/detalles", methods=['GET', 'POST'])
+def detalles():
+	create_form=forms.UserForm(request.form)
+	if request.method=='GET':
+		id=request.args.get('id')
+		# select * from alumnos where id == id
+		alum1 = db.session.query(Alumnos).filter(Alumnos.id==id).first()
+		id=request.args.get('id')
+		nombre=alum1.nombre
+		apaterno=alum1.apaterno
+		email=alum1.email
+	
+	return render_template('detalles.html', id=id, nombre=nombre,apaterno=apaterno, email=email)
 
 if __name__ == '__main__':
 	csrf.init_app(app)
